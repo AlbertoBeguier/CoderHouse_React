cdc0ea8ee1e732f5c07295a852ec74e147d8858e
Alberto Beguier <aabeguier@hotmail.com>
2024-04-20 22:26:43 -0300

    conexion a BD1

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,27 @@ import { ItemListContainer } from "./components/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer";
 import { CartProvider } from "./components/CartContext";
 import { CartDisplay } from "./components/CartDisplay";
+import { getFirestore, getDocs, collection } from "firebase/firestore"; //! Importamos las funciones necesarias para interactuar con la base de datos
+import { useEffect } from "react";
 
 function App() {
+  //! useEffect para probar la conexión a la base de datos
+  useEffect(() => {
+    const db = getFirestore(); // Inicializamos la base de datos
+    const refCollection = collection(db, "items"); // Referencia a la colección completa de la BD
+    getDocs(refCollection).then(snapShot => {
+      if (snapShot.size === 0) console.log("No hay resultados");
+      else {
+        console.log(
+          snapShot.docs.map(doc => {
+            //! Mapeamos los documentos de la colección
+            return { id: doc.id, ...doc.data() };
+          })
+        );
+      }
+    });
+  }, []);
+  //! fin useEffect para probar la conexión a la base de datos
   return (
     <CartProvider>
       <BrowserRouter>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+// Importa las funciones que necesitas de los SDKs que requieras
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// Añade SDKs para los productos de Firebase que quieras usar
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// La configuración de Firebase de tu aplicación web
+// Para el SDK de JS de Firebase v7.20.0 y posteriores, measurementId es opcional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5bQlkaHMn_vuiiqWiN-pAz7nFYgjOYNE",
+  authDomain: "basededatos-aab.firebaseapp.com",
+  projectId: "basededatos-aab",
+  storageBucket: "basededatos-aab.appspot.com",
+  messagingSenderId: "892035320669",
+  appId: "1:892035320669:web:775c2e18b35ece92d476b4",
+  measurementId: "G-QHL2YQEVYX",
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
